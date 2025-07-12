2fdd1091f011736d8239a2bc2fa677d8b3349854
Joaquin Diaz <dev@joacod.com>
2025-07-12 12:15:43 -0300

    todo crud urls

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Todo
+from .forms import TodoForm
 
-# Create your views here.
+# List all todos
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'todo/todo_list.html', {'todos': todos})
+
+# Create a new todo
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo/todo_form.html', {'form': form})
+
+# Update an existing todo
+def todo_update(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todo/todo_form.html', {'form': form})
+
+# Delete a todo
+def todo_delete(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('todo_list')
+    return render(request, 'todo/todo_confirm_delete.html', {'todo': todo})
