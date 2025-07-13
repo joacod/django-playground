1d7967b837d33c605d821139c2553c2bd56d7aae
Joaquin Diaz <dev@joacod.com>
2025-07-13 13:44:05 -0300

    new models registration

--- pizza/admin.py
+++ pizza/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer, Pizza, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Pizza)
+admin.site.register(Order)
+admin.site.register(OrderItem)
